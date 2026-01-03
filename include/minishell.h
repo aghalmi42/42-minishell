4a19869cd4c12dac4b1d98153cb66fa10cea60c4
Amir Ghalmi <aghalmi@f1br7s15.clusters.42paris.fr>
2026-01-03 19:12:10 +0100

    add librairy  to header

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aghalmi <aghalmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 17:14:38 by aghalmi           #+#    #+#             */
-/*   Updated: 2026/01/03 18:20:20 by aghalmi          ###   ########.fr       */
+/*   Updated: 2026/01/03 19:10:24 by aghalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,10 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <signal.h>
 #endif
