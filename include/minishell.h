183369ffa129103cd49f85ec4068a9dee0d12fcd
Amir Ghalmi <aghalmi@f1br7s15.clusters.42paris.fr>
2026-01-03 19:28:12 +0100

    add header minishell

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amoderan <amoderan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aghalmi <aghalmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 17:14:38 by aghalmi           #+#    #+#             */
-/*   Updated: 2026/01/03 19:11:34 by amoderan         ###   ########.fr       */
+/*   Updated: 2026/01/03 19:27:55 by aghalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 # define MINISHELL_H
 
 # include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <string.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include <termios.h>
-# include <unistd.h>
 # include <sys/types.h>
 # include <sys/stat.h>
-# include <dirent.h>
-# include <string.h>
-# include <stdlib.h>
 # include <sys/ioctl.h>
+# include <sys/wait.h>
+# include <dirent.h>
+# include <termios.h>
 # include <curses.h>
 # include <term.h>
+# include <signal.h>
 
 #endif
